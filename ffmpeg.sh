a44d8903ba9ab9c5f2aeed8b72451d6e5cdc0aaa
Moe <github@m03.ca>
2018-09-16 17:06:30 -0700

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 echo "============="
-echo "Install FFMPEG"
-echo "What architecture for FFMPEG do you require?"
-echo "If you don't know check your CPU specs for a hint."
-echo "- 32bit"
-echo "- 64bit"
-echo "- armel-32bit"
-echo "- armhf-32bit"
+echo "Installer for Static FFMPEG with CUDA"
+echo "============="
+echo "Which build of FFMPEG do you require?"
+echo "1. 32bit"
+echo "2. 64bit"
+echo "3. armel-32bit"
+echo "4. armhf-32bit"
+echo "========"
+echo "Default is 64bit (2)"
 read ffmpegbuild
-wget "https://s3.amazonaws.com/cloudcamio/ffmpeg-release-$ffmpegbuild-static.tar.xz"
-tar xf "ffmpeg-release-$ffmpegbuild-static.tar.xz"
-mv "ffmpeg-3.3-$ffmpegbuild-static/ffmpeg" "/usr/bin/ffmpeg"
-mv "ffmpeg-3.3-$ffmpegbuild-static/ffmpeg-10bit" "/usr/bin/ffmpeg-10bit"
-mv "ffmpeg-3.3-$ffmpegbuild-static/ffprobe" "/usr/bin/ffprobe"
-mv "ffmpeg-3.3-$ffmpegbuild-static/ffserver" "/usr/bin/ffserver"
-chmod +x /usr/bin/ffmpeg
-chmod +x /usr/bin/ffmpeg-10bit
-chmod +x /usr/bin/ffprobe
-chmod +x /usr/bin/ffserver
-rm -rf "ffmpeg-3.3-$ffmpegbuild-static"
-rm -rf "ffmpeg-release-$ffmpegbuild-static.tar.xz"
+if [ "$ffmpegbuild" = "" ]; then
+    echo "Selected Default : 64bit"
+    ffmpegbuild="2"
+fi
+case $ffmpegbuild in
+"1")
+ffmpegbuild="32bit"
+  ;;
+"2")
+ffmpegbuild="64bit"
+  ;;
+"3")
+ffmpegbuild="armel-32bit"
+  ;;
+"4")
+ffmpegbuild="armhf-32bit"
+  ;;
+*)
+  echo "Choice not found."
+  ;;
+esac
+wget "https://cdn.shinobi.video/installers/ffmpeg-release-$ffmpegbuild-static.tar.xz"
+tar xf "./ffmpeg-release-$ffmpegbuild-static.tar.xz"
+chmod +x ./ffmpeg-3.3.4-$ffmpegbuild-static/ff*
+mv ./ffmpeg-3.3.4-$ffmpegbuild-static/ff* "/usr/bin/"
+rm -rf "./ffmpeg-3.3.4-$ffmpegbuild-static"
+rm -rf "./ffmpeg-release-$ffmpegbuild-static.tar.xz"
